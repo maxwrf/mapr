02c3609617d2c5e640f178f87d397cdd0041f47b
Max Würfel <maxwuerfel24@gmail.com>
2019-08-28 17:51:36 +0200

    map

--- app/javascript/plugins/init_mapbox.js
+++ app/javascript/plugins/init_mapbox.js
@@ -24,12 +24,44 @@ const fitMapToMarkers = (map, markers) => {
   map.fitBounds(bounds, { padding: 70, maxZoom: 15 });
 };
 
+const addRouteToMap = (map, markers) => {
+  console.log(markers);
+  const coordinatesArr = [];
+  markers.forEach(marker => coordinatesArr.push(`${marker["lat"]},${marker["lng"]}`));
+  const coordinatesString = coordinatesArr.join(';');
+  console.log(coordinatesString);
+  $.get(`https://api.mapbox.com/directions/v5/mapbox/driving/${coordinatesString}?access_token=${mapboxgl.accessToken}&geometries=geojson`, data => {
+
+    map.addLayer({
+      id: 'route',
+      type: 'line',
+      source: {
+        type: 'geojson',
+        data: {
+          type: 'Feature',
+          properties: {},
+          geometry: data.routes[0].geometry,
+        },
+      },
+      layout: {
+        'line-join': 'round',
+        'line-cap': 'round',
+      },
+      paint: {
+        'line-color': '#ff7e5f',
+        'line-width': 8,
+      },
+    })
+  })
+};
+
 const initMapbox = () => {
   if (mapElement) {
     const map = buildMap();
     const markers = JSON.parse(mapElement.dataset.markers);
     addMarkersToMap(map, markers);
     fitMapToMarkers(map, markers);
+    addRouteToMap(map, markers);
   }
 };
 
