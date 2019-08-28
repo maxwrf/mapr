34468deea6213ce6e70274736931032a16239127
Max Würfel <maxwuerfel24@gmail.com>
2019-08-28 19:50:16 +0200

    directions halfway

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -1,27 +1,26 @@
 import "bootstrap";
 import "daemonite-material/js/material.js";
-import 'mapbox-gl/dist/mapbox-gl.css';
 import { initMapbox } from '../plugins/init_mapbox';
 
 initMapbox();
 
-const test = () => {
-  b1.classList.add("btn-primary");
-  b1.classList.remove("btn-secondary");
-}
+// const test = () => {
+//   b1.classList.add("btn-primary");
+//   b1.classList.remove("btn-secondary");
+// }
 
-const activities = document.getElementById("activities");
-const b1 = document.getElementById("b1");
+// const activities = document.getElementById("activities");
+// const b1 = document.getElementById("b1");
 
-b1.addEventListener("click", (event) => {
-  fetch("api/v1/activities")
-  .then(response => response.json())
-  .then((data) => {
-    console.log(data);
-    activities.innerHTML = '';
-    data.forEach((activity) => {
-      activities.insertAdjacentHTML("beforeend",
-        `<li>${activity.name}<br>${activity.description}<br>${activity.address}</li><br>` );
-    });
-  });
-});
+// b1.addEventListener("click", (event) => {
+//   fetch("api/v1/activities")
+//   .then(response => response.json())
+//   .then((data) => {
+//     console.log(data);
+//     activities.innerHTML = '';
+//     data.forEach((activity) => {
+//       activities.insertAdjacentHTML("beforeend",
+//         `<li>${activity.name}<br>${activity.description}<br>${activity.address}</li><br>` );
+//     });
+//   });
+// });
--- app/javascript/plugins/init_mapbox.js
+++ app/javascript/plugins/init_mapbox.js
@@ -1,4 +1,5 @@
 import mapboxgl from 'mapbox-gl';
+import 'mapbox-gl/dist/mapbox-gl.css';
 
 const mapElement = document.getElementById('map');
 
@@ -25,43 +26,58 @@ const fitMapToMarkers = (map, markers) => {
 };
 
 const addRouteToMap = (map, markers) => {
-  console.log(markers);
+  // console.log(markers);
   const coordinatesArr = [];
   markers.forEach(marker => coordinatesArr.push(`${marker["lat"]},${marker["lng"]}`));
   const coordinatesString = coordinatesArr.join(';');
-  console.log(coordinatesString);
-  $.get(`https://api.mapbox.com/directions/v5/mapbox/driving/${coordinatesString}?access_token=${mapboxgl.accessToken}&geometries=geojson`, data => {
-
+  // console.log(coordinatesString);
+  const url = `https://api.mapbox.com/directions/v5/mapbox/driving/${coordinatesString}?access_token=${mapboxgl.accessToken}&geometries=geojson`;
+  const request = new XMLHttpRequest();
+  request.responseType = 'json';
+  request.open('GET', url, true);
+  request.onload  = function() {
+    var jsonResponse = request.response;
+    var distance = jsonResponse.routes[0].distance*0.001;
+    var duration = jsonResponse.routes[0].duration/60;
+    var coords = jsonResponse.routes[0].geometry;
+    console.log(distance);
+    console.log(duration);
+    console.log(coords)
     map.addLayer({
-      id: 'route',
-      type: 'line',
-      source: {
-        type: 'geojson',
-        data: {
-          type: 'Feature',
-          properties: {},
-          geometry: data.routes[0].geometry,
-        },
+      "id": "route",
+      "type": "line",
+      "source": {
+        "type": "geojson",
+        "data": {
+          "type": "Feature",
+          "properties": {},
+          "geometry": coords
+        }
       },
-      layout: {
-        'line-join': 'round',
-        'line-cap': 'round',
+      "layout": {
+        "line-join": "round",
+        "line-cap": "round"
       },
-      paint: {
-        'line-color': '#ff7e5f',
-        'line-width': 8,
-      },
-    })
-  })
+      "paint": {
+        "line-color": "#3b9ddd",
+        "line-width": 8,
+        "line-opacity": 0.8
+      }
+    });
+  }
+  request.send();
 };
 
 const initMapbox = () => {
   if (mapElement) {
     const map = buildMap();
+    map.on("load", () => {
     const markers = JSON.parse(mapElement.dataset.markers);
     addMarkersToMap(map, markers);
     fitMapToMarkers(map, markers);
-    addRouteToMap(map, markers);
+      addRouteToMap(map, markers);
+    })
+
   }
 };
 
