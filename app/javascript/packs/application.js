935e789712ed48a6b8f8fa4fc2202c10392a010f
Max Würfel <maxwuerfel24@gmail.com>
2019-08-30 16:23:02 +0200

    firstpresi

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -5,7 +5,7 @@ import Picker from "pickerjs";
 import  "pickerjs/dist/picker.css";
 import 'mapbox-gl/dist/mapbox-gl.css';
 import { initMapbox } from '../plugins/init_mapbox';
-import { flatpickr } from '../plugins/flatpickr';
+import { initPicker } from '../plugins/flatpickr';
 import { initGmap } from '../plugins/gmaps';
 import { initPlaces } from '../plugins/init_autocomplete.js';
 import {picker} from '../plugins/timepicker.js';
@@ -20,7 +20,7 @@ $(document).ready(function(){
 initGmap();
 initPlaces();
 picker();
-flatpickr();
+initPicker();
 
 const activities_page = document.getElementById("activities_page");
 if (activities_page) {
--- app/javascript/plugins/flatpickr.js
+++ app/javascript/plugins/flatpickr.js
@@ -1,8 +1,17 @@
 import flatpickr from "flatpickr"
 import "flatpickr/dist/themes/airbnb.css" // Note this is important!
 
+
+const go = () => {
 flatpickr(".datepicker", {
   altInput: true
 });
+}
 
-export { flatpickr };
+const initPicker = () => {
+  if (document.querySelectorAll('.datepicker')) {
+    go()
+  };
+};
+
+export { initPicker };
