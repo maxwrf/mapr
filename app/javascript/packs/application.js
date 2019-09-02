b88e16b4021895225a34a0e03dc5b058d8f87543
Max Würfel <maxwuerfel24@gmail.com>
2019-09-02 11:28:17 +0200

    animation on scroll

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -8,7 +8,8 @@ import { initMapbox } from '../plugins/init_mapbox';
 import { initPicker } from '../plugins/flatpickr';
 import { initGmap } from '../plugins/gmaps';
 import { initPlaces } from '../plugins/init_autocomplete.js';
-import {picker} from '../plugins/timepicker.js';
+import { picker } from '../plugins/timepicker.js';
+import { AosInit } from '../plugins/aos.js'
 
 $(document).ready(function(){
   $(".category-choice").click(function(){
@@ -21,6 +22,7 @@ initGmap();
 initPlaces();
 picker();
 initPicker();
+AosInit();
 
 const activities_page = document.getElementById("activities_page");
 if (activities_page) {
