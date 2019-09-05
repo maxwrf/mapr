d1fd19255a8a38da3e7dc450d9ef212c8dd5fb10
Carl Bentham <carlbentham@gmail.com>
2019-09-05 17:51:25 +0200

    added console output in api service

--- app/controllers/api/v1/activities_controller.rb
+++ app/controllers/api/v1/activities_controller.rb
@@ -5,6 +5,7 @@ class Api::V1::ActivitiesController < Api::V1::BaseController
     # e.g. url http://localhost:3000/plans/1/api/v1/activities
     @activities = policy_scope(Activity)
     fetcher = ActivitiesFetcherService.new
+    p "****** PARAMS = #{params}"
     @activities = fetcher.fetch(params).sort { |a, b|  b[:average_rating] <=> a[:average_rating] }
     html = render_to_string partial: 'activities/activity_cards.html.erb', locals: { activities: @activities }
     render json: { activities: @activities, html: html }
